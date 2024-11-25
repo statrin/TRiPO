2b3943be4e9fa87d2aed3346ac42b7ee31ab7800
GARIN <128015447+statrin@users.noreply.github.com>
2024-11-25 16:49:00 +0900

    Update travel.py
    Update import Streamlit

--- travel.py
+++ travel.py
@@ -22,7 +22,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain.chat_models import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
+import streamlit as st
 output_parser = StrOutputParser()
 
 import pandas as pd
